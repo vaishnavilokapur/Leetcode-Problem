04aa7308bc39e82be721a6a140b2af94597e4c0b
Vaishnavi Lokapur <vaishulk066@gmail.com>
2026-08-12 16:33:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.93%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,8 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        clean_list=[]
-        for num in nums:
-            if num!=val:
-                clean_list.append(num)
-        for i in range(len(clean_list)):
-             nums[i]=clean_list[i]
-        return len(clean_list)
-        
+        slow=0
+        for fast in range(len(nums)):
+            if nums[fast]!=val:
+                nums[slow]=nums[fast]
+                slow +=1
+        return slow
